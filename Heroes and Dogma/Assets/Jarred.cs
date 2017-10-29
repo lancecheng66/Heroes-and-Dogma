5ab867380f18b8d7513c3bb5488dc6eff852ac75
lancecheng <lancecheng214@yahoo.com>
2017-10-30 01:28:07 +0800

    Working on Jarred's Character
    Created Cast button so that he can teleport

---
+++ Heroes and Dogma/Assets/Jarred.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Jarred : Control
+{
+   
+
+    public override void HandleInput() // where we put in controls (we can use this to make 2-3 player games
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MyAnimator.SetTrigger("jump");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            MyAnimator.SetTrigger("attack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            MyAnimator.SetTrigger("slide");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            MyAnimator.SetBool("crouch", true);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            MyAnimator.SetTrigger("throw");
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            MyAnimator.SetTrigger("cast");
+        }
+
+    }
+   
+   
+}
+
--- Heroes and Dogma/Assets/Kayla Scripts/Kayla.cs
+++ Heroes and Dogma/Assets/Kayla Scripts/Kayla.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public delegate void DeadEventHandlerKayla();
+
 public class Kayla : Control
 {
     [SerializeField]
