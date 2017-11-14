fa2c869f02521e56bd16db268397bf3172fd5289
lancecheng <lancecheng214@yahoo.com>
2017-11-15 02:27:41 +0800

    Working on Character Selection

---
+++ Heroes and Dogma/Assets/CharacterSelect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelect : MonoBehaviour
+{
+
+    private GameObject[] characterList;
+
+    private void Start()
+    {
+        characterList = new GameObject[transform.childCount];
+        // fill array with models
+        for (int i = 0; i< transform.childCount; i++)
+            characterList[i] = transform.GetChild(i).gameObject;
+        // turn off their renderer
+        foreach (GameObject go in characterList)        
+            go.SetActive(false);
+
+        // toggle on the selected index
+    }
+
+
+    public void Kayla()
+    {
+        characterList[0].SetActive(true);
+    }
+
+    public void Jarred()
+    {
+        characterList[1].SetActive(true);
+    }
+
+    public void ConfirmButton()
+    {
+        SceneManager.LoadScene("Stage 1");
+    }
+	
+}
